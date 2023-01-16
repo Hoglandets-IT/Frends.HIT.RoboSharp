1ed12376ece87caf54310c420e9e2051a40f642b
Lars Scheibling <lars@scheibling.se>
2023-01-16 14:33:17 +0000

    Init

---
+++ Frends.HIT.RoboSharp.Tests/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using Frends.HIT.RoboSharp;
+
+namespace Frends.HIT.RoboSharp.Tests
+{
+    public class Program 
+    {
+        static void Main (string[] args)
+        {
+            General.TestSync(
+                "HMGMT04",
+                "",
+                ""
+            );
+        }
+    }
+}
