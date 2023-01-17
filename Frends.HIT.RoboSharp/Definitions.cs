a6c5848710ec644b229782314105868faf45d78b
scheibling <24367830+scheibling@users.noreply.github.com>
2023-01-17 16:03:35 +0100

    Finished initial version

--- Frends.HIT.RoboSharp/Definitions.cs
+++ Frends.HIT.RoboSharp/Definitions.cs
@@ -1,104 +1,230 @@
-﻿// using System.ComponentModel;
-// using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Common;
+using System.Reflection;
+using System.Text;
 
-// // This should be the root namespace of the package
-// namespace Frends.HIT.RoboSharp;
+namespace Frends.HIT.RoboSharp {
+    public enum RoboExitCode : ushort {
+        NoFilesCopied = 0,
+        AllFilesCopied = 1,
+        AdditionalFilesInDestinationDirectoryNoFilesCopied = 2,
+        SomeFilesCopiedAdditionalFilesPresent = 3,
+        SomeFilesCopiedSomeFilesMismatched = 5,
+        AdditionalFilesAndMismatchedFilesExistNoFilesCopied = 6,
+        FilesCopiedMismatchAndAdditionalFilesPresent = 7,
+        SeveralFilesDidNotCopy = 8
+    }
 
-// public enum MultipleChoiceOption
-// {
-//     /// <summary>
-//     /// This is the text that is shown in the control panel when hovering over the option
-//     /// </summary>
-//     [Display(Name = "Option A Display Name")]
-//     OptionA,
-    
-//     /// <summary>
-//     /// This is the text that is shown in the control panel when hovering over the option
-//     /// </summary>
-//     [Display(Name = "Option B Display Name")]
-//     OptionB,
-    
-//     /// <summary>
-//     /// This is the text that is shown in the control panel when hovering over the option
-//     /// </summary>
-//     [Display(Name = "Option C Display Name")]
-//     OptionC,
-// }
+    public enum RunWithShell {
+        [Display(Name = "Powershell")]
+        powershell,
 
-// /// <summary>
-// /// This is the information shown about the input group in the control panel
-// /// Since this is set via the interface and not programmatically, it doesn't need a public class for instantiating.
-// /// </summary>
-// public class TaskInput
-// {
-//     /// <summary>
-//     /// This is the information shown about the parameter in the control panel
-//     /// </summary>
-//     [Display(Name = "Multiple Choice Option Name")]
-//     public MultipleChoiceOption ChoiceOption { get; set; }
-    
-//     /// <summary>
-//     /// This is the information shown about the text parameter in the panel
-//     /// If no text is provided, the default value is "ABCDEFG" (also shown in control panel)
-//     /// </summary>
-//     [DefaultValue("ABCDEFG")]
-//     [Display(Name = "Text Parameter Name")]
-//     public string NameOption { get; set; }
-    
-//     /// <summary>
-//     /// The information shown about this parameter in the panel.
-//     /// With the PasswordPropertyText attribute, the content is hidden by default
-//     /// </summary>
-//     [PasswordPropertyText]
-//     public string PasswordOption { get; set; }
-    
-//     /// <summary>
-//     /// The default editor box for this parameter will be of the "Expression" type
-//     /// </summary>
-//     [DisplayFormat(DataFormatString = "Expression")]
-//     public string ExpressionTextOption { get; set; }
-    
-//     /// <summary>
-//     /// A boolean option
-//     /// </summary>
-//     public bool BooleanOption { get; set; }
-    
-//     /// <summary>
-//     /// This field will only be shown if the "ChoiceOption" field is A
-//     /// </summary>
-//     [UIHint(nameof(ChoiceOption), "", MultipleChoiceOption.OptionA, MultipleChoiceOption.OptionB)]
-//     public string HiddenTextOption { get; set; }
-    
-//     /// <summary>
-//     /// This field will only be shown if the BooleanOption field is false
-//     /// </summary>
-//     [UIHint(nameof(BooleanOption), "", false)]
-//     public string HiddenBooleanOption { get; set; }
-// }
+        [Display(Name = "Pwsh")]
+        pwsh,
 
-// /// <summary>
-// /// This is the output from the task.
-// /// Since this will be defined programmatically, there is an upside to using
-// /// a public facing class for creating an instance for the output. 
-// /// </summary>
-// public class TaskOutput
-// {
-//     /// <summary>
-//     /// Whether the operation was successful
-//     /// </summary>
-//     public bool Success { get; set; }
-    
-//     /// <summary>
-//     /// Any messages returned by the task
-//     /// </summary>
-//     public string Info { get; set; }
+        [Display(Name = "Cmd")]
+        cmd,
+    }
 
-//     public TaskOutput(
-//         bool success,
-//         string info
-//     )
-//     {
-//         Success = success;
-//         Info = info;
-//     }
-// }
+    public enum RoboOption {
+        [Display(Name = "Mirror Directory Tree (/MIR)")]
+        MIR,
+
+        [Display(Name = "Copy Subdirectories (/E)")]
+        E,
+
+        [Display(Name = "Wait for share names to be defined (/TBD)")]
+        TBD,
+
+        [Display(Name = "Do not log file names (/NFL)")]
+        NFL,
+
+        [Display(Name = "Do not log directory names (/NDL)")]
+        NDL,
+
+        [Display(Name = "Do not show progress bar (/NP)")]
+        NP,
+    }
+    public class PathSettings {
+        /// <summary>
+        /// The path, without a trailing slash
+        /// </summary>
+        [Display(Name = "Path")]
+        [DefaultValue(@"\\SOMESERVER\SomeShare$\SomeFolder")]
+        [DisplayFormat(DataFormatString = "Text")]
+        [Required]
+        public string Path { get; set; }
+
+        /// <summary>
+        /// The username to use for the path (e.g. INTERN\f-user)
+        /// </summary>
+        [Display(Name = @"DOMAIN\Username")]
+        [DefaultValue(@"DOMAIN\Username")]
+        [DisplayFormat(DataFormatString = "Text")]
+        [Required]
+        public string Username { get; set; }
+
+        /// <summary>
+        /// The password for the user
+        /// </summary>
+        [Display(Name = "Password")]
+        [DisplayFormat(DataFormatString = "Expression")]
+        [Required]
+        public string Password { get; set; }
+    }
+
+    public class SyncParameters
+    {
+        /// <summary>
+        /// The shell to use for the sync
+        /// </summary>
+        [Display(Name = "Shell")]
+        [DefaultValue(RunWithShell.powershell)]
+        public RunWithShell Shell { get; set; }
+
+        /// <summary>
+        /// A list of files to exclude
+        /// </summary>
+        [Display(Name = @"Excluded Files")]
+        [DefaultValue(new string[] { "Thumbs.db", "desktop.ini" })]
+        public string[] ExcludeFiles { get; set; }
+        
+        /// <summary>
+        /// A list of folders to exclude
+        /// </summary>
+        [Display(Name = @"Excluded Folders")]
+        [DefaultValue(null)]
+        public string[] ExcludeFolders { get; set; }
+
+        /// <summary>
+        /// Number of retries on failed copies
+        /// </summary>
+        [Display(Name = "Retry Count")]
+        [DefaultValue("10")]
+        public string RetryCount { get; set; }
+
+        /// <summary>
+        /// Number of seconds to wait between retries
+        /// </summary>
+        [Display(Name = "Retry Wait Time")]
+        [DefaultValue("30")]
+        public string RetryWaitTime { get; set; }
+
+        /// <summary>
+        /// A list of options to enable for the sync
+        /// </summary>
+        [Display(Name = "RoboCopy Options")]
+        [DefaultValue(new RoboOption[] { 
+            RoboOption.MIR, 
+            RoboOption.NFL,
+            RoboOption.NDL,
+            RoboOption.NP
+        })]
+        public RoboOption[] Options { get; set; }
+    }
+
+    public class InfoMatch {
+        public string Match { get; set; }
+        public bool GetStatistics { get; set; }
+        public bool GetStatisticsBytes { get; set; }
+    }
+
+    public class SyncItemInfo {
+        public int Total { get; set; }
+        public int Copied { get; set; }
+        public int Skipped { get; set; }
+        public int Mismatch { get; set; }
+        public int Failed { get; set; }
+        public int Extras { get; set; }
+    }
+
+    public class SyncExitInfo {
+        public RoboExitCode ExitCode { get; set; }
+        public string ExitMessage { get; set; }
+
+        public string Source { get; set; }
+        public string Destination { get; set; }
+
+        
+        public string Options { get; set; }
+        public SyncItemInfo Dirs { get; set; } = null;    
+        public SyncItemInfo Files { get; set; } = null;    
+        public SyncItemInfo Bytes { get; set; } = null;
+
+        public string Started { get; set; }
+        public string Ended { get; set; }
+    }
+    
+
+    public class Helpers {
+        public static InfoMatch[] GetReturnInfo = new InfoMatch[]{
+                new InfoMatch{
+                    Match = "Dirs",
+                    GetStatistics = true,
+                    GetStatisticsBytes = false
+                },
+                new InfoMatch{
+                    Match = "Files",
+                    GetStatistics = true,
+                    GetStatisticsBytes = false
+                },
+                new InfoMatch{
+                    Match = "Bytes",
+                    GetStatistics = true,
+                    GetStatisticsBytes = true
+                },
+                new InfoMatch{
+                    Match = "Started",
+                    GetStatistics = false,
+                    GetStatisticsBytes = false
+                },
+                new InfoMatch{
+                    Match = "Ended",
+                    GetStatistics = false,
+                    GetStatisticsBytes = false
+                },
+                new InfoMatch{
+                    Match = "Options",
+                    GetStatistics = false,
+                    GetStatisticsBytes = false
+                }
+            };
+        public static SyncItemInfo GetStatistics(string row, bool Bytes = false) {
+            string[] desc = row.Split(new char[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
+            if (Bytes) {
+                desc[1] = desc[1].Replace(" m", "m");
+                desc[1] = desc[1].Replace(" g", "g");
+                desc[1] = desc[1].Replace(" k", "k");
+                desc[1] = desc[1].Replace(" b", "b");
+            }
+
+            string[] parts = desc[1].Split(new char[]{' ', '\t'}, StringSplitOptions.RemoveEmptyEntries);
+
+            if (parts.Length > 0 && !Bytes) {
+                return new SyncItemInfo() {
+                    Total = int.Parse(parts[0]),
+                    Copied = int.Parse(parts[1]),
+                    Skipped = int.Parse(parts[2]),
+                    Mismatch = int.Parse(parts[3]),
+                    Failed = int.Parse(parts[4]),
+                    Extras = int.Parse(parts[5]),
+                };
+            }
+            else if (parts.Length > 0 && Bytes) {
+                return new SyncItemInfo() {
+                    Total = int.Parse(parts[0].TrimEnd('m', 'g', 'k', 'b').Split('.')[0]),
+                    Copied = int.Parse(parts[1].TrimEnd('m', 'g', 'k', 'b').Split('.')[0]),
+                    Skipped = int.Parse(parts[2].TrimEnd('m', 'g', 'k', 'b').Split('.')[0]),
+                    Mismatch = int.Parse(parts[3].TrimEnd('m', 'g', 'k', 'b').Split('.')[0]),
+                    Failed = int.Parse(parts[4].TrimEnd('m', 'g', 'k', 'b').Split('.')[0]),
+                    Extras = int.Parse(parts[5].TrimEnd('m', 'g', 'k', 'b').Split('.')[0]),
+                };
+            }
+            return new SyncItemInfo();
+        }
+    }
+    
+}
---
+++ Frends.HIT.RoboSharp/Frends.HIT.RoboSharp.cs
@@ -0,0 +1,107 @@
+﻿using System.Text.RegularExpressions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Frends.HIT.RoboSharp {
+    public class Main {
+        public static SyncExitInfo SyncFolders(PathSettings source, PathSettings destination, [PropertyTab]SyncParameters parameters) {
+            // Create the authentication commands
+            List<string> AuthCommand = new List<string>(){
+                "net use \"" + source.Path + "\" \"" + source.Password + "\" /user:" + source.Username + ";",
+                "net use \"" + destination.Path + "\" \"" + destination.Password + "\" /user:" + destination.Username + ";"
+            };
+
+            // Assemble the robocopy command parameters
+            List<string> RoboCommand = new List<string>(){
+                "robocopy \"" + source.Path + "\" \"" + destination.Path + "\""
+            };
+
+            // Add the options
+            if (parameters.Options != null) {
+                foreach (RoboOption option in parameters.Options) {
+                    RoboCommand.Add("/" + option.ToString());
+                }
+            }
+
+            // Add the retry count
+            if (parameters.RetryCount != null) {
+                RoboCommand.Add("/R:" + parameters.RetryCount);
+                if (parameters.RetryWaitTime != null) {
+                    RoboCommand.Add("/W:" + parameters.RetryWaitTime);
+                }
+            }
+
+            // Add the exclude files
+            if (parameters.ExcludeFiles != null) {
+                RoboCommand.Add("/XF \"" + String.Join(" ", parameters.ExcludeFiles) + "\"");
+            }
+
+            // Add the exclude folders
+            if (parameters.ExcludeFolders != null) {
+                RoboCommand.Add("/XD \"" + String.Join(" ", parameters.ExcludeFolders) + "\"");
+            }
+
+            // Set shell-based parameters
+            string ShellParams = "";
+            if (parameters.Shell == RunWithShell.powershell || parameters.Shell == RunWithShell.pwsh) {
+                ShellParams = "-NoProfile -ExecutionPolicy Bypass -Command ";
+            }
+            else {
+                ShellParams = "/C ";
+            }
+
+            // Create a process
+            System.Diagnostics.Process process = new System.Diagnostics.Process{
+                StartInfo = new System.Diagnostics.ProcessStartInfo{
+                    FileName = parameters.Shell.ToString() + ".exe",
+                    Arguments = ShellParams + String.Join(" ", AuthCommand) + String.Join(" ", RoboCommand),
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+            
+            process.Start();
+            SyncExitInfo exInfo = new SyncExitInfo(); 
+
+            while (!process.StandardOutput.EndOfStream) {
+                string line = process.StandardOutput.ReadLine().TrimStart(new char[]{' ', '\t'});
+                if (line.StartsWith("ERROR")) {
+                    throw new Exception(line);
+                }
+                if (line != "") {
+                    Console.WriteLine(line);
+                    foreach (var retn in Helpers.GetReturnInfo)
+                    {
+                        if (retn.GetStatistics && line.Substring(0, retn.Match.Length+2) == retn.Match+" :" && !line.Contains("*.*")) {
+                            Console.WriteLine("Match: " + retn.Match);
+                            exInfo.GetType().GetProperty(retn.Match).SetValue(
+                                exInfo, 
+                                Helpers.GetStatistics(line, retn.GetStatisticsBytes)
+                            );
+                        }
+                        else if (!retn.GetStatistics && line.Substring(0, retn.Match.Length+2) == retn.Match+" :") {
+                            Console.WriteLine("Match 2: " + retn.Match);
+                            exInfo.GetType().GetProperty(retn.Match).SetValue(
+                                exInfo, 
+                                line.Substring(retn.Match.Length+2)
+                            );
+                        }
+                    }
+                }
+            }
+
+            // Get exit code from RoboExitCode by number
+            // Get name of exit code
+            exInfo.ExitCode = (RoboExitCode)process.ExitCode;
+            exInfo.ExitMessage = Regex.Replace(exInfo.ExitCode.ToString(), "(\\B[A-Z])", " $1");
+
+            // Close the process
+            process.Close();
+
+            // Return the exit code
+            return exInfo;
+        }
+    }
+}
