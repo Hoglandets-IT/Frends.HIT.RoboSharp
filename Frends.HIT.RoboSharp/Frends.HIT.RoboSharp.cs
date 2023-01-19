a81d4c26b4fca80148c51500c57097080f3d3a20
scheibling <24367830+scheibling@users.noreply.github.com>
2023-01-19 17:27:10 +0100

    Bugfix - trying to access process after closing Added extended error reporting/handling

--- Frends.HIT.RoboSharp/Frends.HIT.RoboSharp.cs
+++ Frends.HIT.RoboSharp/Frends.HIT.RoboSharp.cs
@@ -19,82 +19,93 @@ namespace Frends.HIT.RoboSharp {
         /// <returns>SyncExitInfo Object</returns>
         [DisplayName("Sync SMB Folders")]
         public static SyncExitInfo SyncFolders([PropertyTab]TargetSettings target, [PropertyTab]SyncParameters parameters) {
-            // Create the authentication commands
-            List<string> AuthCommand = new List<string>(){
-                "net use \"" + target.Source.Path + "\" \"" + target.Source.Password + "\" /user:" + target.Source.Username + ";",
-                "net use \"" + target.Destination.Path + "\" \"" + target.Destination.Password + "\" /user:" + target.Destination.Username + ";"
-            };
+            SyncExitInfo exInfo = new SyncExitInfo(){
+                Source = target.Source.Path,
+                Destination = target.Destination.Path
+            }; 
 
-            // Create the robocopy command with parameters
-            string RoboCommand = "robocopy \"" + target.Source.Path + "\" \"" + target.Destination.Path + "\" /NP" + parameters.GetAdditionalFlags();
+            try {
 
-            // Set shell-based parameters
-            string ShellParams = "";
-            if (parameters.Shell == RunWithShell.powershell || parameters.Shell == RunWithShell.pwsh) {
-                ShellParams = "-NoProfile -ExecutionPolicy Bypass -Command ";
-            }
-            else {
-                ShellParams = "/C ";
-            }
-
-            // Create a process
-            System.Diagnostics.Process process = new System.Diagnostics.Process{
-                StartInfo = new System.Diagnostics.ProcessStartInfo{
-                    FileName = parameters.Shell.ToString() + ".exe",
-                    Arguments = ShellParams + String.Join(" ", AuthCommand) + String.Join(" ", RoboCommand),
-                    UseShellExecute = false,
-                    RedirectStandardOutput = true,
-                    CreateNoWindow = true
-                }
-            };
             
-            process.Start();
-            SyncExitInfo exInfo = new SyncExitInfo(); 
-            List<string> FullLog = new List<string>();
 
-            while (!process.StandardOutput.EndOfStream) {
-                string line = process.StandardOutput.ReadLine().TrimStart(new char[]{' ', '\t'});
-                if (line.StartsWith("ERROR")) {
-                    FullLog.Add(line);                    
+                // Create the authentication commands
+                List<string> AuthCommand = new List<string>(){
+                    "net use \"" + target.Source.Path + "\" \"" + target.Source.Password + "\" /user:" + target.Source.Username + ";",
+                    "net use \"" + target.Destination.Path + "\" \"" + target.Destination.Password + "\" /user:" + target.Destination.Username + ";"
+                };
+
+                // Create the robocopy command with parameters
+                string RoboCommand = "robocopy \"" + target.Source.Path + "\" \"" + target.Destination.Path + "\" /NP" + parameters.GetAdditionalFlags();
+
+                // Set shell-based parameters
+                string ShellParams = "";
+                if (parameters.Shell == RunWithShell.powershell || parameters.Shell == RunWithShell.pwsh) {
+                    ShellParams = "-NoProfile -ExecutionPolicy Bypass -Command ";
                 }
-                if (line != "") {
-                    FullLog.Add(line);
-                    foreach (var retn in Helpers.GetReturnInfo)
-                    {
-                        if (retn.GetStatistics && line.StartsWith(retn.Match + " :") && !line.Contains("*.*")) {
-                            exInfo.GetType().GetProperty(retn.Match).SetValue(
-                                exInfo, 
-                                Helpers.GetStatistics(line, retn.GetStatisticsBytes)
-                            );
-                        }
-                        else if (!retn.GetStatistics && line.StartsWith(retn.Match + " :")) {
-                            exInfo.GetType().GetProperty(retn.Match).SetValue(
-                                exInfo, 
-                                line.Substring(retn.Match.Length+2)
-                            );
+                else {
+                    ShellParams = "/C ";
+                }
+
+                exInfo.Command = parameters.Shell.ToString() + ".exe" + ShellParams + String.Join(" ", RoboCommand);
+
+                // Create a process
+                System.Diagnostics.Process process = new System.Diagnostics.Process{
+                    StartInfo = new System.Diagnostics.ProcessStartInfo{
+                        FileName = parameters.Shell.ToString() + ".exe",
+                        Arguments = ShellParams + String.Join(" ", AuthCommand) + String.Join(" ", RoboCommand),
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        CreateNoWindow = true
+                    }
+                };
+                
+                process.Start();
+
+                while (!process.StandardOutput.EndOfStream) {
+                    string line = process.StandardOutput.ReadLine().TrimStart(new char[]{' ', '\t'});
+                    if (line.StartsWith("ERROR")) {
+                        exInfo.FullLog.Add(line);                    
+                    }
+                    if (line != "") {
+                        exInfo.FullLog.Add(line);
+                        foreach (var retn in Helpers.GetReturnInfo)
+                        {
+                            if (retn.GetStatistics && line.StartsWith(retn.Match + " :") && !line.Contains("*.*")) {
+                                exInfo.GetType().GetProperty(retn.Match).SetValue(
+                                    exInfo, 
+                                    Helpers.GetStatistics(line, retn.GetStatisticsBytes)
+                                );
+                            }
+                            else if (!retn.GetStatistics && line.StartsWith(retn.Match + " :")) {
+                                exInfo.GetType().GetProperty(retn.Match).SetValue(
+                                    exInfo, 
+                                    line.Substring(retn.Match.Length+2)
+                                );
+                            }
                         }
                     }
                 }
+
+                // Get exit code from RoboExitCode by number
+                // Get name of exit code
+                // Set other exit parameters
+                exInfo.ExitCode = (RoboExitCode)process.ExitCode;
+                exInfo.ExitMessage = Regex.Replace(exInfo.ExitCode.ToString(), "(\\B[A-Z])", " $1");
+
+                // Close the process
+                process.Close();
+            }
+            catch {
+                exInfo.ExitCode = RoboExitCode.SeriousError;
+                exInfo.ExitMessage = "Serious Error Occured - Check Log";
             }
 
-            // Get exit code from RoboExitCode by number
-            // Get name of exit code
-            // Set other exit parameters
-            exInfo.ExitCode = (RoboExitCode)process.ExitCode;
-            exInfo.ExitMessage = Regex.Replace(exInfo.ExitCode.ToString(), "(\\B[A-Z])", " $1");
-            exInfo.Source = target.Source.Path;
-            exInfo.Destination = target.Destination.Path;
-            exInfo.Command = String.Join(" ", RoboCommand);
-            exInfo.FullLog = FullLog;
-            // Close the process
-            process.Close();
-
-            if (process.ExitCode == (Int32)RoboExitCode.SeriousError) {
-                throw new Exception(JsonConvert.SerializeObject(exInfo));
+            if (exInfo.ExitCode == RoboExitCode.SeriousError) {
+                    throw new Exception(JsonConvert.SerializeObject(exInfo));
             }
-
+            
             // Return the exit code
-            return exInfo;
+            return exInfo;            
         }
     }
 }
